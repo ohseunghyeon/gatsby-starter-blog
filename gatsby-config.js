4aa13afb615b30adbd7289c9c352718c15fc4a05
ohseunghyeon <postercolo@gmail.com>
2019-09-04 17:40:55 +0900

    Add dependency - gatsby-remark-embed-video

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,8 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          "gatsby-remark-embed-video",
+          "gatsby-remark-responsive-iframe",
           {
             resolve: `gatsby-remark-images`,
             options: {
