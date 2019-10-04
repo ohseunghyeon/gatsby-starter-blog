59e03644014928d82b70e8bf95b14a6b3fd2c126
ohseunghyeon <postercolo@gmail.com>
2019-10-04 21:06:22 +0900

    Apply headroom to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "gatsby"
+import Headroom from "headroom.js"
 
 import "./index.scss"
 import Hamburger from "../HeaderHamburger"
 import Nav from "../HeaderNav"
 
 const Header = ({ title, location }) => {
+  const refHeader = useRef();
+  const headroom = useRef();
+
+  useEffect(() => {
+    headroom.current = new Headroom(refHeader.current);
+    headroom.current.init();
+    return () => headroom.current.destroy();
+  }, [])
+
   return (
-    <header className="header">
+    <header ref={refHeader} className="header">
       <Hamburger />
       <Link className="header-logo" to={`/`}>{title}</Link>
       <Nav location={location} />
