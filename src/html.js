e013de65a84dfdf18de12a873fd54ae5c65c8e45
victor.oh <victor.oh@adriel.ai>
2020-02-20 17:13:12 +0900

    Delete fb script

--- src/html.js
+++ src/html.js
@@ -46,26 +46,6 @@ export default function HTML(props) {
 
                 setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
               })();
-
-              window.fbAsyncInit = function () {
-                FB.init({
-                  appId: '307062716427571',
-                  cookie: true,
-                  xfbml: true,
-                  version: 'v6.0'
-                });
-          
-                FB.AppEvents.logPageView();
-          
-              };
-          
-              (function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) { return; }
-                js = d.createElement(s); js.id = id;
-                js.src = "https://connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));
             `,
           }}
         />
